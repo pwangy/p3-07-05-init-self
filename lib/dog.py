2b0fd539e2e9d39e6b12df0c3bb24e8e9cb0651c
pwangy <blipsandclicks@gmail.com>
2024-03-16 22:52:40 -0700

    complete codealong

--- lib/dog.py
+++ lib/dog.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Dog:
-    pass
+    
+    def __init__(self, name, breed="Mutt", favorite_toy="Any"):
+        self.name = name
+        self.favorite_toy = favorite_toy
+        self.breed = breed
+        # print(f"{name} who is a {breed} has been born!")
+
+    def bark(self):
+        print(f"Woof!")
+
+    def showing_self(self):
+        return self
+
+    def get_adopted(self, owner_name):
+        self.owner = owner_name
+
+
+fido = Dog("Fido", "Dalmantion")
+
+snoopy = Dog("Snoopy", "Beagle")
+snoopy.showing_self()
+snoopy is snoopy.showing_self()
+snoopy.get_adopted("Chloe")
+snoopy.owner
